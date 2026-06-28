18c7510ac64fcb6f8261f331e6deee143f6c735b
wizard113 <yeong.rok.lee@gmail.com>
2026-06-28 09:56:52 +0900

    gemini v11

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,31 +9,6 @@ export default function Home() {
           <h3>Next.js app service github action 코드 배포4=================</h3>
           <h3>Next.js app service github action 코드 배포4=================</h3>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
       </main>
     </div>
   );
