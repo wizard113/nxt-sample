a1bf4a1b0113bfe4ab0e97f1a405662ad20b3836
wizard113 <yeong.rok.lee@gmail.com>
2026-06-28 10:35:32 +0900

    gemini-2 v6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.intro}>
           <h3>Next.js app service github action 코드 배포4=================</h3>
-          <h3>Next.js app service github action 코드 배포4=================</h3>
+          <h3>Next.js app service github action 코드 배포5=================</h3>
         </div>
       </main>
     </div>
